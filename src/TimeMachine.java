3449859e65098ddf4ed0fbfef8d514b7a9d7fa3c
agatito <agatito@yopmail.com>
2018-01-15 18:48:02 +0100

    person

---
+++ src/TimeMachine.java
@@ -0,0 +1,14 @@
+public class TimeMachine {
+
+    void blacklnTime (Person person) {
+
+        person.age--;
+
+    }
+
+    void timeTravel (Person person, int ag) {
+
+        person.age= person.age-ag;
+
+    }
+}
